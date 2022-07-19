d6dfe3037060d44a3c6d05b57f5630f51b8d033d
ARTEM PESHKOV <barracuda8906@mail.ru>
2022-07-19 15:37:15 +0200

    19 July. end of school lesson

--- src/register/Register.java
+++ src/register/Register.java
@@ -5,4 +5,14 @@ public interface Register {
 
     public void removePerson(Person person);
 
+    public int getSize();
+
+    public Person getPerson(int index);
+
+    public Person findPersonByPhoneNumber(String phoneNumber) throws PersonNotFoundException;
+
+    public Person findPersonByName(String name) throws PersonNotFoundException;
+
+    public void handler(Throwable e);
+
 }
--- src/register/SetRegister.java
+++ src/register/SetRegister.java
@@ -1,39 +1,39 @@
 package register;
 
-import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
-public class ListRegister implements Register {
-    private List<Person> list;
+public class SetRegister implements Register{
+    private Set<Person> set;
 
-    public ListRegister(List<Person> list) {
-        this.list = list;
+    public SetRegister(Set<Person> set) {
+        this.set = set;
     }
 
     @Override
     public void addPerson(Person person) {
-        list.add(person);
+        set.add(person);
     }
 
     @Override
     public void removePerson(Person person) {
-        list.remove(person);
+    set.remove(person);
     }
 
     @Override
     public int getSize() {
-        return list.size();
+        return set.size();
     }
 
     @Override
     public Person getPerson(int index) {
-        return list.get(index);
+        return null;
     }
 
-    @Override
     public Person findPersonByPhoneNumber(String phoneNumber) throws PersonNotFoundException {
         Person person = null;
-        for (Person p : list) {
+        for (Person p : set) {
             if (phoneNumber.equals(p.getPhoneNumber()))
                 person = p;
         }
@@ -41,13 +41,15 @@ public class ListRegister implements Register {
         if (person == null)
             throw new PersonNotFoundException("Person with this phone was not found");
 
-        return person;
+        return set.stream()
+                .filter(p -> p.getPhoneNumber().equals(phoneNumber))
+                .findFirst()
+                .get();
     }
-
     @Override
     public Person findPersonByName(String name) throws PersonNotFoundException {
         Person person = null;
-        for (Person p : list) {
+        for (Person p : set) {
             if (p.getName().equals(name)) {
                 person = p;
                 break;
@@ -59,16 +61,8 @@ public class ListRegister implements Register {
         return person;
     }
 
-
     @Override
     public void handler(Throwable e) {
-        System.out.println(e.getMessage());
-        ConsoleUI ui = new ConsoleUI(this);
-        ui.run();
-    }
 
-    public void print() {
-        list.stream().forEach(System.out::println);
-        }
     }
-
+}
